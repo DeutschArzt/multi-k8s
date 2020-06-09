1fb749bdaa82e6cafb5490ee7caaa7f6963d0ec5
Matthew Buhr <mjbuhr@MJB-MacBook-Pro-2019-16.local>
2020-06-09 14:16:21 -0500

    Reformatting text on app.js page

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,7 @@ function App() {
       <div className="App">
         <header>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            I finally got Kubernetes to work! ...or did I? Beep. Boop.
-          </p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>I finally got Kubernetes to work! ...or did I? Beep. Boop.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -25,8 +20,12 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/otherpage">Other Page</Link>
+          </div>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
